10b14e7d82cd663f10d664fb565879248fde0fcb
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-07-14 00:21:32 +0530

    Added mixins and prepared mobile first development

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,17 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvar = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('stylestask', function() {
     return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, nested, cssvar, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles/'));
+        .pipe(postcss([cssImport, mixins, nested, cssvar, autoprefixer]))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles/'));
+
 });
 
