0339b0b9f712e6de46896a770182910d0fc19589
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-07-13 22:11:16 +0530

    Completed till Gulp Timeout

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'), 
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir : "app"
+        }
+    });
+    watch('./app/index.html', function(){
+        browserSync.reload();
+    });
+    watch('./app/assets/**/*.css', gulp.series('stylestask','cssInject'));
+});
+
+gulp.task('cssInject', function() {
+    return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
+});
